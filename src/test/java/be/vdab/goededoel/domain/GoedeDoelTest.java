eee03228bb7c70e6ea89ee5f6d07a97af33d38dd
sve313 <sve313@gmail.com>
2020-10-27 19:59:22 +0100

    JaCoCo

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -6,19 +6,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GoedeDoelTest {
-	private static final String NAAM = "CliniClowns";
-	private GoedeDoel doel;
+    private static final String NAAM = "CliniClowns";
+    private GoedeDoel doel;
 
-	@BeforeEach
-	void beforeEach() {
-		doel = new GoedeDoel(NAAM);
-	}
+    @BeforeEach
+    void beforeEach() {
+        doel = new GoedeDoel(NAAM);
+    }
 
-	@Test
-	void getNaam() {
-		assertThat(doel.getNaam()).isEqualTo(NAAM);
-	}
+    @Test
+    void getNaam() {
+        assertThat(doel.getNaam()).isEqualTo(NAAM);
+    }
 
-	@Test void eenNieuwDoelHeeftNogGeenOpbrengst() { assertThat(doel.getOpgebracht()).isZero(); }
+    @Test
+    void eenNieuwDoelHeeftNogGeenOpbrengst() {
+        assertThat(doel.getOpgebracht()).isZero();
+    }
 
 }
