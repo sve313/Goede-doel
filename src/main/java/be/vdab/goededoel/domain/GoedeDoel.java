efcefa91c1a8d3ce315d2f8286f634faf67986b8
sve313 <sve313@gmail.com>
2020-10-27 20:21:54 +0100

    8 SPOTBUGS

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -3,27 +3,32 @@ package be.vdab.goededoel.domain;
 import java.math.BigDecimal;
 
 public class GoedeDoel {
-	private final String naam;
-	private BigDecimal opgebracht = BigDecimal.ZERO;
+    private final String naam;
+    private BigDecimal opgebracht = BigDecimal.ZERO;
 
-	public GoedeDoel(String naam) {
-		this.naam = naam;
-	}
+    public GoedeDoel(String naam) {
+        this.naam = naam;
+    }
 
-	public String getNaam() {
-		return naam;
-	}
+    public String getNaam() {
+        return naam;
+    }
 
-	public BigDecimal getOpgebracht() {
-		return opgebracht;
-	}
+    public BigDecimal getOpgebracht() {
+        return opgebracht;
+    }
 
-	@Override
-	public boolean equals(Object object) {
-		if (object instanceof GoedeDoel) {
-			var ander = (GoedeDoel) object;
-			return naam.equalsIgnoreCase(ander.naam);
-		}
-		return false;
-	}
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof GoedeDoel) {
+            var ander = (GoedeDoel) object;
+            return naam.equalsIgnoreCase(ander.naam);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.naam.toUpperCase().hashCode();
+    }
 }
--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -41,4 +41,9 @@ class GoedeDoelTest {
         assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
     }
 
+    @Test
+    void gelijkeDoelGevenHebbenDezelfdeHashCode() {
+        assertThat(doel).hasSameHashCodeAs(new GoedeDoel(NAAM));
+    }
+
 }
